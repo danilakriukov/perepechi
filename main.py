1ff31fde6902c3c44b12b3378fd028719b313d4f
Danila Kriukov <2018.danilakriukov@gmail.com>
2022-09-11 19:03:36 +0500

    added boilercode

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+from flask import Flask, render_template, request, redirect, url_for
+import time
+from datetime import datetime
+import pickle
 
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def begin():
+	return redirect(url_for('main'))
+
+
+@app.route('/main')
+def main():
+	return render_template("main.html")
+
+
+if __name__ == "__main__":
+	app.run()
